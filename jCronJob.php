f376c1e96aef40ce0cdac91bf1a445a3857d3c59
jayesh2018 <jayeshmarathe2009@gmail.com>
2020-06-20 17:13:45 +0530

    cron job updated

--- jCronJob.php
+++ jCronJob.php
@@ -1,8 +1,9 @@
 <?php
 
+date_default_timezone_set("asia/kolkata");
 require('./shopgold.xyz/connection.php');
 
-$insertWinnerHistoryQuery = "INSERT INTO winner_history (color,digit) (SELECT color,digit FROM winner WHERE color <> '' AND digit <> 0 )";
+$insertWinnerHistoryQuery = "INSERT INTO winner_history (color,digit,room) (SELECT color,digit,room FROM winner WHERE color <> '' AND digit <> 0 )";
 $result = mysqli_query($conn,$insertWinnerHistoryQuery);
 echo $insertWinnerHistoryQuery . $result . "\n<br>";
 
@@ -10,12 +11,17 @@ $truncateWinnerQuery = "TRUNCATE TABLE winner";
 $result = mysqli_query($conn,$truncateWinnerQuery);
 echo $truncateWinnerQuery . $result . "\n<br>";
 
-$insertWinnerQuery = "INSERT INTO winner (game_date) VALUES(ADDTIME(now(),'12:33:00'))";
-// $insertWinnerQuery = "INSERT INTO winner (game_date) VALUES(ADDTIME(now() + INTERVAL 3 MINUTE)";
+// $insertWinnerQuery = "INSERT INTO winner (game_date) VALUES(ADDTIME(now(),'12:33:00'))";
+$dateTime = new DateTime();
+$timezone = new DateTimeZone('Asia/Kolkata');
+$dateTime->setTimezone($timezone);
+$dateTime->modify('+3 minutes');
+$cDate = $dateTime->format('Y-m-d H:i:s');
+$insertWinnerQuery = "INSERT INTO winner (room,game_date) VALUES('picky','$cDate'),('parity','$cDate'),('spare','$cDate'),('bacone','$cDate')";
 $result = mysqli_query($conn,$insertWinnerQuery);
 echo $insertWinnerQuery . $result . "\n<br>";
 
-$insertPurchareHistoryQuery = "INSERT INTO purchased_history (customer_id,choice,money) (SELECT customer_id,choice,money FROM purchased)";
+$insertPurchareHistoryQuery = "INSERT INTO purchased_history (customer_id,choice,money,room) (SELECT customer_id,choice,money,room FROM purchased)";
 $result = mysqli_query($conn,$insertPurchareHistoryQuery);
 echo $insertPurchareHistoryQuery . $result . "\n<br>";
 
