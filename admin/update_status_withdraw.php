a1e15a878860932354c16a6335f83b4eb73f5ce0
jayesh2018 <jayeshmarathe2009@gmail.com>
2020-06-21 12:24:29 +0530

    withdraw status col removed

--- admin/update_status_withdraw.php
+++ admin/update_status_withdraw.php
@@ -1,30 +1,26 @@
 <?php
 require('../connection.php');
 
-if($_POST['id']!="")
-{
-    $id=mysqli_real_escape_string($conn,$_POST['id']);
-    $updateid=mysqli_real_escape_string($conn,$_POST['updateid']);
-	$arr=array();
+if ($_POST['id'] != "") {
+	$id = mysqli_real_escape_string($conn, $_POST['id']);
+	$updateid = mysqli_real_escape_string($conn, $_POST['updateid']);
+	$arr = array();
 
 
-	$result=mysqli_query($conn,"update withdrawl_request set  status='$id' where user_id='$updateid'");
-	if($result>0)
-	{
-        $arr['status']=1;
-          $msg="Accepted";$class="badge-success";
-        if($id==2){
-          $msg="Rejected";$class="badge-danger";
-        }
-        $arr['html']="<span class='badge ".$class."'>".$msg."</span>";
-       
+	$result = mysqli_query($conn, "update withdrawl_request set status='$id' where user_id='$updateid'");
+	// $result = 1;
+	if ($result > 0) {
+		$arr['status'] = 1;
+		$msg = "Accepted";
+		$class = "badge-success";
+		if ($id == 2) {
+			$msg = "Rejected";
+			$class = "badge-danger";
+		}
+		$arr['html'] = "<span class='badge " . $class . "'>" . $msg . "</span>";
+	} else {
+		$arr['status'] = 0;
 	}
-	else
-	{
-		$arr['status']=0;
-	}
-
 }
 header('Content-Type:application/json');
 echo json_encode($arr);
- ?>
--- admin/withdrawl.php
+++ admin/withdrawl.php
@@ -1,8 +1,7 @@
-<?php 
+<?php
 session_start();
 require('../connection.php');
-if(!isset($_SESSION['admin']))
-{
+if (!isset($_SESSION['admin'])) {
   echo '<script>
   location.href="login.php";
   </script>';
@@ -12,187 +11,109 @@ $result = mysqli_query($conn, $query);
 
 ?>
 <html>
-    <head>
-        <title>Withdrawl</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <style>
+
+<head>
+  <title>Withdrawl</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <style>
     .my-custom-scrollbar {
-        position: relative;
-        height:500px;
-        overflow: auto;
-        }
-        .table-wrapper-scroll-y {
-        display: block;
-        }
-}
-    </style>
-    </head>
-    <body>
-        <div class="container text-center p-4">
-            <h2>Withdrawl Request</h2>
+      position: relative;
+      height: 500px;
+      overflow: auto;
+    }
 
-        </div>
-        <div class="container">
-            <div class="table-wrapper-scroll-y my-custom-scrollbar"> 
-                <table class="table table-hover table-bordered table-striped text-center">
-                    <thead>
-                      <tr>
-                        
-                        <th scope="col">Username</th>
-                        <th scope="col">Amount</th>
-                        <th scope="col">Status</th>
-                        <th scope="col">Action</th>
-                        
-                        
-                      </tr>
-                    </thead>
-                    <tbody>
-                    <?php
-				if ($result->num_rows > 0) {
+    .table-wrapper-scroll-y {
+      display: block;
+    }
+  </style>
+</head>
 
-					while($row = $result->fetch_object()) {
-            $msg="Pending";$class="badge-warning";
-            if($row->status==1)
-            {
-              $msg="Accepted";$class="badge-success";
-            }
-            if($row->status==2){
-              $msg="Rejected";$class="badge-danger";
-            }
-            
+<body>
+  <div class="container text-center p-4">
+    <h2>Withdrawl Request</h2>
 
-            $status="<span class='badge ".$class."'>".$msg."</span>";
-            ?>
-            <tr>
-                            <td><?=$row->name;?></td>
-                            <td><?=$row->amount;?></td>
-                            <td id="tdchng_<?=$row->user_id;?>" ><?=$status;?> </td>
-                            <td><button type="button" class="btn btn-success Req" data-updateid="<?=$row->user_id;?>" data-id="1">Allow</button> &nbsp <button type="button" class="btn btn-danger Req" data-id="2" data-updateid="<?=$row->user_id;?>">Deny</button></td>
-                          </tr>
-            <?php
-          }}
+  </div>
+  <div class="container">
+    <div class="table-wrapper-scroll-y my-custom-scrollbar">
+      <table class="table table-hover table-bordered table-striped text-center">
+        <thead>
+          <tr>
+
+            <th scope="col">Username</th>
+            <th scope="col">Amount</th>
+            <!-- <th scope="col">Status</th> -->
+            <th scope="col">Action</th>
+
+
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          if ($result->num_rows > 0) {
+            while ($row = $result->fetch_object()) {
           ?>
-                        <!-- <tr>
-                            <td>Mark</td>
-                            <td>1000</td>
-                            <td><button type="button" class="btn btn-success">Allow</button> &nbsp <button type="button" class="btn btn-danger">Deny</button></td>
-                          </tr>
-                          <tr>
-                            <td>Sergy</td>
-                            <td>400000</td>
-                            <td><button type="button" class="btn btn-success">Allow</button> &nbsp <button type="button" class="btn btn-danger">Deny</button></td>
-                          </tr>
-                          <tr>
-                            <td>Larry </td>
-                            <td>2000</td>
-                            <td><button type="button" class="btn btn-success">Allow</button> &nbsp <button type="button" class="btn btn-danger">Deny</button></td>
-                          </tr>
-                          <tr>
-                            <td>Mark</td>
-                            <td>1000</td>
-                            <td><button type="button" class="btn btn-success">Allow</button> &nbsp <button type="button" class="btn btn-danger">Deny</button></td>
-                          </tr>
-                          <tr>
-                            <td>Sergy</td>
-                            <td>400000</td>
-                            <td><button type="button" class="btn btn-success">Allow</button> &nbsp <button type="button" class="btn btn-danger">Deny</button></td>
-                          </tr>
-                          <tr>
-                            <td>Larry </td>
-                            <td>2000</td>
-                            <td><button type="button" class="btn btn-success">Allow</button> &nbsp <button type="button" class="btn btn-danger">Deny</button></td>
-                          </tr>
-                      <tr>
-                        <td>Mark</td>
-                        <td>1000</td>
-                        <td><button type="button" class="btn btn-success">Allow</button> &nbsp <button type="button" class="btn btn-danger">Deny</button></td>
-                      </tr>
-                      <tr>
-                        <td>Sergy</td>
-                        <td>400000</td>
-                        <td><button type="button" class="btn btn-success">Allow</button> &nbsp <button type="button" class="btn btn-danger">Deny</button></td>
-                      </tr>
-                      <tr>
-                        <td>Larry </td>
-                        <td>2000</td>
-                        <td><button type="button" class="btn btn-success">Allow</button> &nbsp <button type="button" class="btn btn-danger">Deny</button></td>
-                      </tr>
-                      <tr>
-                        <td>Mark</td>
-                        <td>1000</td>
-                        <td><button type="button" class="btn btn-success">Allow</button> &nbsp <button type="button" class="btn btn-danger">Deny</button></td>
-                      </tr>
-                      <tr>
-                        <td>Sergy</td>
-                        <td>400000</td>
-                        <td><button type="button" class="btn btn-success">Allow</button> &nbsp <button type="button" class="btn btn-danger">Deny</button></td>
-                      </tr>
-                      <tr>
-                       
-                        <td>Larry </td>
-                        <td>2000</td>
-                        <td><button type="button" class="btn btn-success">Allow</button> &nbsp <button type="button" class="btn btn-danger">Deny</button></td>
-                      </tr>
-                      <tr>
-                       
-                        <td>Sergy</td>
-                        <td>400000</td>
-                        <td><button type="button" class="btn btn-success">Allow</button> &nbsp <button type="button" class="btn btn-danger">Deny</button></td>
-                      </tr>
-                      <tr>
-                       
-                        <td>Sergy</td>
-                        <td>400000</td>
-                        <td><button type="button" class="btn btn-success">Allow</button> &nbsp <button type="button" class="btn btn-danger">Deny</button></td>
-                      </tr>
-                      <tr>
-                       
-                        <td>Sergy</td>
-                        <td>400000</td>
-                        <td><button type="button" class="btn btn-success">Allow</button> &nbsp <button type="button" class="btn btn-danger">Deny</button></td>
-                      </tr>
-                      <tr>
-                       
-                        <td>Sergy</td>
-                        <td>400000</td>
-                        <td><button type="button" class="btn btn-success">Allow</button> &nbsp <button type="button" class="btn btn-danger">Deny</button></td>
-                      </tr> -->
-                    </tbody>
-                  </table>
-            </div>
-           
-        </div>
+              <tr>
+                <td class="jName" data-updateid=<?= $row->user_id; ?>><?= $row->name; ?></td>
+                <td><?= $row->amount; ?></td>
+                <!-- <td id="tdchng_<?= $row->user_id; ?>"><?= $status; ?> </td> -->
+                <td><button type="button" class="btn btn-success Req" data-updateid="<?= $row->user_id; ?>" data-id="1">Allow</button> &nbsp <button type="button" class="btn btn-danger Req" data-id="2" data-updateid="<?= $row->user_id; ?>">Deny</button></td>
+              </tr>
+          <?php
+            }
+          }
+          ?>
+          <!-- <tr>
+                <td>Mark</td>
+                <td>1000</td>
+                <td><button type="button" class="btn btn-success">Allow</button> &nbsp <button type="button" class="btn btn-danger">Deny</button></td>
+              </tr>
+          <tr>
+            <td>Sergy</td>
+            <td>400000</td>
+            <td><button type="button" class="btn btn-success">Allow</button> &nbsp <button type="button" class="btn btn-danger">Deny</button></td>
+          </tr> -->
+        </tbody>
+      </table>
+    </div>
 
-        
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-<script>
-$(document).ready(function(){
+  </div>
 
-  $('.Req').click(function(){
-    var id=$(this).data('id');
-    var updateid=$(this).data('updateid');
-    var req=$('.Req');
-    //update_status_withdraw.php
-    req.prop('disabled',true);
-    $.ajax({
-      url:"update_status_withdraw.php",
-      type:"post",
-      data:{id:id,updateid:updateid},
-      success:function(data){
-        
-        if(data.status==1)
-        {
-          req.prop('disabled',false);
-          $('#tdchng_'+updateid).html(data.html);
-        }
-      }
 
+  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  <script>
+    $(document).ready(function() {
+      $('.jName').click(function() {
+        console.log($(this).html());
       });
-  });
+      $('.Req').click(function() {
+        var id = $(this).data('id');
+        var updateid = $(this).data('updateid');
+        var req = $(this);
+        req.prop('disabled', true);
+        $.ajax({
+          url: "update_status_withdraw.php",
+          type: "post",
+          data: {
+            id: id,
+            updateid: updateid
+          },
+          success: function(data) {
 
-});
-</script>
-    </body>
+            if (data.status == 1) {
+              req.prop('disabled', false);
+              req.parent().html(data.html);
+              console.log(data.html);
+              // $('#tdchng_' + updateid).html(data.html);
+            }
+          }
+
+        });
+      });
+
+    });
+  </script>
+</body>
+
 </html>
